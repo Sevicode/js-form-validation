9417efbc6b7059d0c45f59a9033c2c32cc8865d5
Sevda <sevda.amini@gmail.com>
2022-03-07 20:02:55 +1000

    validation

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const form = document.getElementById("form")
-const username = document.getElementById("name-id")
-const email = document.getElementById("email-id")
-const password = document.getElementById("password-id")
-const password2 = document.getElementById("password2-id")
+const username = document.getElementById("username")
+const email = document.getElementById("email")
+const password = document.getElementById("password")
+const password2 = document.getElementById("password2")
 
 // always add the event listener to the form instead of submit button
 // because at the end of the day this is the form which is being submitted
@@ -24,11 +24,29 @@ const showSuccess = (input) => {
     formControl.className = "form-control success"
 }
 
-// Check required 
+// Check required function
 const checkRequired = (inputArr) => {
     inputArr.forEach((input) => {
-        console.log(input)
+        if(input.value.length === 0) {
+            // if section also can be written if(input.value.trim()==='')
+            // Also, ${input.id.charAt(0).toUpperCase()+input.id.slice(1)} can be written as a separate function
+            showError(input, `${input.id.charAt(0).toUpperCase()+input.id.slice(1)} is required`)
+        }else{
+            showSuccess(input)
+        }
     })
+}
+
+
+// Check length function
+const checkLength = (input, min, max) => {
+if(input.value.length < min) {
+    showError(input, `${input.id.charAt(0).toUpperCase()+input.id.slice(1)} must be at least ${min} characters`)
+}else if(input.value.length > max){
+    showError(input, `${input.id.charAt(0).toUpperCase()+input.id.slice(1)} must be less than ${max} characters`)
+}else{
+    showSuccess(input)
+}
 }
 
 
@@ -39,5 +57,7 @@ const checkRequired = (inputArr) => {
 form.addEventListener('submit', e => {
     e.preventDefault()
     checkRequired([username, email,password,password2 ])
+    checkLength(username, 5, 8)
+    checkLength(password, 6, 30)
     
 })
